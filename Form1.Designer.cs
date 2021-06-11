d014837cbe7d69c9b9811608639d6e36d811cb3f
Loris <loris.filliger@gmail.com>
2021-06-11 19:05:25 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace Calculatrice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bu_aq = new System.Windows.Forms.Button();
+            this.l_valeur1 = new System.Windows.Forms.Label();
+            this.l_valeur2 = new System.Windows.Forms.Label();
+            this.bu_calc = new System.Windows.Forms.Button();
+            this.l_result = new System.Windows.Forms.Label();
+            this.num_valeur1 = new System.Windows.Forms.NumericUpDown();
+            this.num_valeur2 = new System.Windows.Forms.NumericUpDown();
+            this.bu_at = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.bu_cos = new System.Windows.Forms.Button();
+            this.bu_sin = new System.Windows.Forms.Button();
+            this.bu_tan = new System.Windows.Forms.Button();
+            this.bu_back = new System.Windows.Forms.Button();
+            this.l_note = new System.Windows.Forms.Label();
+            this.l_cst = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.num_valeur1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_valeur2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bu_aq
+            // 
+            this.bu_aq.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_aq.Location = new System.Drawing.Point(0, 0);
+            this.bu_aq.Name = "bu_aq";
+            this.bu_aq.Size = new System.Drawing.Size(153, 44);
+            this.bu_aq.TabIndex = 0;
+            this.bu_aq.Text = "Aire quadrilatère";
+            this.toolTip1.SetToolTip(this.bu_aq, "PErmet dfe calculer une aire d\'un quadrilatère");
+            this.bu_aq.UseVisualStyleBackColor = true;
+            this.bu_aq.Click += new System.EventHandler(this.bu_aq_Click);
+            // 
+            // l_valeur1
+            // 
+            this.l_valeur1.AutoSize = true;
+            this.l_valeur1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_valeur1.Location = new System.Drawing.Point(109, 153);
+            this.l_valeur1.Name = "l_valeur1";
+            this.l_valeur1.Size = new System.Drawing.Size(60, 24);
+            this.l_valeur1.TabIndex = 1;
+            this.l_valeur1.Text = "label1";
+            this.l_valeur1.Visible = false;
+            // 
+            // l_valeur2
+            // 
+            this.l_valeur2.AutoSize = true;
+            this.l_valeur2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_valeur2.Location = new System.Drawing.Point(109, 194);
+            this.l_valeur2.Name = "l_valeur2";
+            this.l_valeur2.Size = new System.Drawing.Size(60, 24);
+            this.l_valeur2.TabIndex = 2;
+            this.l_valeur2.Text = "label2";
+            this.l_valeur2.Visible = false;
+            // 
+            // bu_calc
+            // 
+            this.bu_calc.BackColor = System.Drawing.SystemColors.Info;
+            this.bu_calc.Enabled = false;
+            this.bu_calc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_calc.Location = new System.Drawing.Point(700, 461);
+            this.bu_calc.Name = "bu_calc";
+            this.bu_calc.Size = new System.Drawing.Size(96, 44);
+            this.bu_calc.TabIndex = 5;
+            this.bu_calc.Text = "Calc";
+            this.bu_calc.UseVisualStyleBackColor = false;
+            this.bu_calc.Click += new System.EventHandler(this.bu_calc_Click);
+            // 
+            // l_result
+            // 
+            this.l_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_result.Location = new System.Drawing.Point(63, 328);
+            this.l_result.Name = "l_result";
+            this.l_result.Size = new System.Drawing.Size(473, 52);
+            this.l_result.TabIndex = 0;
+            this.l_result.Click += new System.EventHandler(this.l_result_Click);
+            // 
+            // num_valeur1
+            // 
+            this.num_valeur1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num_valeur1.Location = new System.Drawing.Point(272, 151);
+            this.num_valeur1.Name = "num_valeur1";
+            this.num_valeur1.Size = new System.Drawing.Size(120, 29);
+            this.num_valeur1.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.num_valeur1, "Entre ton nombre ici");
+            this.num_valeur1.Visible = false;
+            this.num_valeur1.ValueChanged += new System.EventHandler(this.num_valeur1_ValueChanged);
+            // 
+            // num_valeur2
+            // 
+            this.num_valeur2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num_valeur2.Location = new System.Drawing.Point(272, 192);
+            this.num_valeur2.Name = "num_valeur2";
+            this.num_valeur2.Size = new System.Drawing.Size(120, 29);
+            this.num_valeur2.TabIndex = 7;
+            this.num_valeur2.Visible = false;
+            this.num_valeur2.ValueChanged += new System.EventHandler(this.num_valeur2_ValueChanged);
+            // 
+            // bu_at
+            // 
+            this.bu_at.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_at.Location = new System.Drawing.Point(159, 0);
+            this.bu_at.Name = "bu_at";
+            this.bu_at.Size = new System.Drawing.Size(123, 44);
+            this.bu_at.TabIndex = 8;
+            this.bu_at.Text = "Aire triangle";
+            this.toolTip1.SetToolTip(this.bu_at, "Permet de calculer l\'aire d\'un triangle");
+            this.bu_at.UseVisualStyleBackColor = true;
+            this.bu_at.Click += new System.EventHandler(this.bu_at_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // bu_cos
+            // 
+            this.bu_cos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_cos.Location = new System.Drawing.Point(640, 0);
+            this.bu_cos.Name = "bu_cos";
+            this.bu_cos.Size = new System.Drawing.Size(48, 44);
+            this.bu_cos.TabIndex = 12;
+            this.bu_cos.Text = "cos";
+            this.toolTip1.SetToolTip(this.bu_cos, "Permet de calculer avec le cosinus");
+            this.bu_cos.UseVisualStyleBackColor = true;
+            this.bu_cos.Click += new System.EventHandler(this.bu_cos_Click);
+            // 
+            // bu_sin
+            // 
+            this.bu_sin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_sin.Location = new System.Drawing.Point(694, 0);
+            this.bu_sin.Name = "bu_sin";
+            this.bu_sin.Size = new System.Drawing.Size(48, 44);
+            this.bu_sin.TabIndex = 13;
+            this.bu_sin.Text = "sin";
+            this.toolTip1.SetToolTip(this.bu_sin, "Permet de calculer avec le sinus");
+            this.bu_sin.UseVisualStyleBackColor = true;
+            // 
+            // bu_tan
+            // 
+            this.bu_tan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_tan.Location = new System.Drawing.Point(748, 0);
+            this.bu_tan.Name = "bu_tan";
+            this.bu_tan.Size = new System.Drawing.Size(48, 44);
+            this.bu_tan.TabIndex = 14;
+            this.bu_tan.Text = "tan";
+            this.toolTip1.SetToolTip(this.bu_tan, "Permet de calculer la tangente");
+            this.bu_tan.UseVisualStyleBackColor = true;
+            // 
+            // bu_back
+            // 
+            this.bu_back.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bu_back.Enabled = false;
+            this.bu_back.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_back.Location = new System.Drawing.Point(12, 461);
+            this.bu_back.Name = "bu_back";
+            this.bu_back.Size = new System.Drawing.Size(96, 44);
+            this.bu_back.TabIndex = 9;
+            this.bu_back.Text = "Retour";
+            this.bu_back.UseVisualStyleBackColor = false;
+            this.bu_back.Click += new System.EventHandler(this.bu_back_Click);
+            // 
+            // l_note
+            // 
+            this.l_note.AutoSize = true;
+            this.l_note.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_note.ForeColor = System.Drawing.Color.Gray;
+            this.l_note.Location = new System.Drawing.Point(560, 360);
+            this.l_note.Name = "l_note";
+            this.l_note.Size = new System.Drawing.Size(51, 20);
+            this.l_note.TabIndex = 15;
+            this.l_note.Text = "label1";
+            this.l_note.Visible = false;
+            // 
+            // l_cst
+            // 
+            this.l_cst.AutoSize = true;
+            this.l_cst.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_cst.Location = new System.Drawing.Point(659, 67);
+            this.l_cst.Name = "l_cst";
+            this.l_cst.Size = new System.Drawing.Size(46, 18);
+            this.l_cst.TabIndex = 17;
+            this.l_cst.Text = "label1";
+            this.l_cst.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 517);
+            this.Controls.Add(this.l_cst);
+            this.Controls.Add(this.l_note);
+            this.Controls.Add(this.bu_tan);
+            this.Controls.Add(this.bu_sin);
+            this.Controls.Add(this.bu_cos);
+            this.Controls.Add(this.bu_back);
+            this.Controls.Add(this.bu_at);
+            this.Controls.Add(this.num_valeur2);
+            this.Controls.Add(this.num_valeur1);
+            this.Controls.Add(this.l_result);
+            this.Controls.Add(this.bu_calc);
+            this.Controls.Add(this.l_valeur2);
+            this.Controls.Add(this.l_valeur1);
+            this.Controls.Add(this.bu_aq);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.num_valeur1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_valeur2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bu_aq;
+        private System.Windows.Forms.Label l_valeur1;
+        private System.Windows.Forms.Label l_valeur2;
+        private System.Windows.Forms.Button bu_calc;
+        private System.Windows.Forms.Label l_result;
+        private System.Windows.Forms.NumericUpDown num_valeur1;
+        private System.Windows.Forms.NumericUpDown num_valeur2;
+        private System.Windows.Forms.Button bu_at;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button bu_back;
+        private System.Windows.Forms.Button bu_cos;
+        private System.Windows.Forms.Button bu_sin;
+        private System.Windows.Forms.Button bu_tan;
+        private System.Windows.Forms.Label l_note;
+        private System.Windows.Forms.Label l_cst;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculatrice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int whereAmI = 0;
+        int fraction = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            l_note.Text = "Note, utilisez :\n-cos Pour calculer le CA ou l'HYP\n-sin Pour calculer le CO ou l'HYP\n-tan Pour calculer le CO ou le CA\nCAH SOH TOA";
+        }
+
+        private void bu_aq_Click(object sender, EventArgs e)
+        {
+            whereAmI = 1;
+
+            l_valeur1.Text = "Longueur";
+            l_valeur2.Text = "Largeur";
+
+            num_valeur1.Text = "0";
+            num_valeur2.Text = "0";
+
+            l_valeur1.Visible = true;
+            l_valeur2.Visible = true;
+            num_valeur1.Visible = true;
+            num_valeur2.Visible = true;
+            bu_back.Enabled = true;
+        }
+
+        private void bu_calc_Click(object sender, EventArgs e)
+        {
+            double valeur1;
+            double valeur2;
+
+            valeur1 = Convert.ToDouble(num_valeur1.Text);
+            valeur2 = Convert.ToDouble(num_valeur2.Text);
+
+            if (whereAmI == 1) 
+            {
+                double result = valeur1 * valeur2;
+                l_result.Text = "Le résultat est : " + Convert.ToString(result) + " cm²";
+            }
+            if (whereAmI == 2)
+            {
+                double result = (valeur1 * valeur2) / 2;
+                l_result.Text = "Le résultat est : " + Convert.ToString(result) + " cm²";
+            }
+
+            l_result.Visible = true;
+        }
+
+        private void num_valeur1_ValueChanged(object sender, EventArgs e)
+        {
+            double valeur1 = 0;
+            double valeur2 = 0;
+
+            if (num_valeur1.Text != "")
+            {
+                valeur1 = Convert.ToDouble(num_valeur1.Text);
+            }
+
+            if (num_valeur2.Text != "")
+            {
+                valeur2 = Convert.ToDouble(num_valeur2.Text);
+            }
+
+            if (valeur1 == 0 || valeur2 == 0)
+            {
+
+                bu_calc.Enabled = false;
+
+            }
+            else
+            {
+                bu_calc.Enabled = true;
+            }
+        }
+
+        private void num_valeur2_ValueChanged(object sender, EventArgs e)
+        {
+            double valeur1 = 0;
+            double valeur2 = 0;
+
+            if (num_valeur1.Text != "")
+            {
+                valeur1 = Convert.ToDouble(num_valeur1.Text);
+            }
+
+            if (num_valeur2.Text != "")
+            {
+                valeur2 = Convert.ToDouble(num_valeur2.Text);
+            }
+
+            if (valeur1 == 0 || valeur2 == 0)
+            {
+
+                bu_calc.Enabled = false;
+
+            }
+            else
+            {
+                bu_calc.Enabled = true;
+            }
+        }
+
+        private void bu_at_Click(object sender, EventArgs e)
+        {
+            whereAmI = 2;
+
+            l_valeur1.Text = "Base";
+            l_valeur2.Text = "Hauteur";
+
+            num_valeur1.Text = "0";
+            num_valeur2.Text = "0";
+
+            l_valeur1.Visible = true;
+            l_valeur2.Visible = true;
+            num_valeur1.Visible = true;
+            num_valeur2.Visible = true;
+        }
+
+        private void l_result_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bu_back_Click(object sender, EventArgs e)
+        {
+            num_valeur1.Text = "0";
+            num_valeur2.Text = "0";
+
+            l_valeur1.Visible = false;
+            l_valeur2.Visible = false;
+            num_valeur1.Visible = false;
+            num_valeur2.Visible = false;
+
+            l_result.Visible = false;
+            l_note.Visible = false;
+            l_cst.Visible = false;
+
+            bu_aq.Visible = true;
+            bu_at.Visible = true;
+            bu_cos.Visible = true;
+            bu_sin.Visible = true;
+            bu_tan.Visible = true;
+        }
+
+        private void bu_cos_Click(object sender, EventArgs e)
+        {
+            l_note.Visible = true;
+
+            bu_aq.Visible = false;
+            bu_at.Visible = false;
+            bu_cos.Visible = false;
+            bu_sin.Visible = false;
+            bu_tan.Visible = false;
+
+            bu_back.Enabled = true;
+            l_cst.Visible = true;
+            du_cst.Visible = true;
+        }
+    }
+}
