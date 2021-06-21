95361a520c145a87ec8c091b72de59c673628ec8
IwaYori <loris.filliger@gmail.com>
2021-06-21 21:09:58 +0200

    version finale propre 1.04 première release

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -56,6 +56,7 @@ namespace Calculatrice
             this.bu_diff = new System.Windows.Forms.Button();
             this.bu_prod = new System.Windows.Forms.Button();
             this.bu_quo = new System.Windows.Forms.Button();
+            this.l_credits = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.num_valeur1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.num_valeur2)).BeginInit();
             this.SuspendLayout();
@@ -63,7 +64,7 @@ namespace Calculatrice
             // bu_aq
             // 
             this.bu_aq.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.bu_aq.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_aq.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_aq.Location = new System.Drawing.Point(0, 0);
             this.bu_aq.Name = "bu_aq";
             this.bu_aq.Size = new System.Drawing.Size(153, 44);
@@ -76,10 +77,10 @@ namespace Calculatrice
             // l_valeur1
             // 
             this.l_valeur1.AutoSize = true;
-            this.l_valeur1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.l_valeur1.Location = new System.Drawing.Point(64, 153);
+            this.l_valeur1.Font = new System.Drawing.Font("Quicksand", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_valeur1.Location = new System.Drawing.Point(64, 199);
             this.l_valeur1.Name = "l_valeur1";
-            this.l_valeur1.Size = new System.Drawing.Size(60, 24);
+            this.l_valeur1.Size = new System.Drawing.Size(59, 28);
             this.l_valeur1.TabIndex = 1;
             this.l_valeur1.Text = "label1";
             this.toolTip1.SetToolTip(this.l_valeur1, "Valeur 1 du calcul");
@@ -88,10 +89,10 @@ namespace Calculatrice
             // l_valeur2
             // 
             this.l_valeur2.AutoSize = true;
-            this.l_valeur2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.l_valeur2.Location = new System.Drawing.Point(64, 194);
+            this.l_valeur2.Font = new System.Drawing.Font("Quicksand", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_valeur2.Location = new System.Drawing.Point(64, 240);
             this.l_valeur2.Name = "l_valeur2";
-            this.l_valeur2.Size = new System.Drawing.Size(60, 24);
+            this.l_valeur2.Size = new System.Drawing.Size(64, 28);
             this.l_valeur2.TabIndex = 2;
             this.l_valeur2.Text = "label2";
             this.toolTip1.SetToolTip(this.l_valeur2, "Valeur 2 du calcul");
@@ -101,7 +102,7 @@ namespace Calculatrice
             // 
             this.bu_calc.BackColor = System.Drawing.SystemColors.Info;
             this.bu_calc.Enabled = false;
-            this.bu_calc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_calc.Font = new System.Drawing.Font("Quicksand", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_calc.Location = new System.Drawing.Point(700, 461);
             this.bu_calc.Name = "bu_calc";
             this.bu_calc.Size = new System.Drawing.Size(96, 44);
@@ -122,10 +123,10 @@ namespace Calculatrice
             // 
             // num_valeur1
             // 
-            this.num_valeur1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num_valeur1.Location = new System.Drawing.Point(272, 151);
+            this.num_valeur1.Font = new System.Drawing.Font("Quicksand", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num_valeur1.Location = new System.Drawing.Point(272, 197);
             this.num_valeur1.Name = "num_valeur1";
-            this.num_valeur1.Size = new System.Drawing.Size(120, 29);
+            this.num_valeur1.Size = new System.Drawing.Size(120, 31);
             this.num_valeur1.TabIndex = 6;
             this.toolTip1.SetToolTip(this.num_valeur1, "Entre ton nombre ici");
             this.num_valeur1.Visible = false;
@@ -134,10 +135,10 @@ namespace Calculatrice
             // 
             // num_valeur2
             // 
-            this.num_valeur2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num_valeur2.Location = new System.Drawing.Point(272, 192);
+            this.num_valeur2.Font = new System.Drawing.Font("Quicksand", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num_valeur2.Location = new System.Drawing.Point(272, 238);
             this.num_valeur2.Name = "num_valeur2";
-            this.num_valeur2.Size = new System.Drawing.Size(120, 29);
+            this.num_valeur2.Size = new System.Drawing.Size(120, 31);
             this.num_valeur2.TabIndex = 7;
             this.toolTip1.SetToolTip(this.num_valeur2, "Entre ton nombre ici");
             this.num_valeur2.Visible = false;
@@ -146,7 +147,7 @@ namespace Calculatrice
             // bu_at
             // 
             this.bu_at.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.bu_at.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_at.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_at.Location = new System.Drawing.Point(159, 0);
             this.bu_at.Name = "bu_at";
             this.bu_at.Size = new System.Drawing.Size(123, 44);
@@ -163,8 +164,8 @@ namespace Calculatrice
             // bu_cos
             // 
             this.bu_cos.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.bu_cos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bu_cos.Location = new System.Drawing.Point(640, 0);
+            this.bu_cos.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_cos.Location = new System.Drawing.Point(640, 1);
             this.bu_cos.Name = "bu_cos";
             this.bu_cos.Size = new System.Drawing.Size(48, 44);
             this.bu_cos.TabIndex = 12;
@@ -176,7 +177,7 @@ namespace Calculatrice
             // bu_sin
             // 
             this.bu_sin.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.bu_sin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_sin.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_sin.Location = new System.Drawing.Point(694, 0);
             this.bu_sin.Name = "bu_sin";
             this.bu_sin.Size = new System.Drawing.Size(48, 44);
@@ -189,7 +190,7 @@ namespace Calculatrice
             // bu_tan
             // 
             this.bu_tan.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.bu_tan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_tan.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_tan.Location = new System.Drawing.Point(748, 0);
             this.bu_tan.Name = "bu_tan";
             this.bu_tan.Size = new System.Drawing.Size(48, 44);
@@ -203,7 +204,7 @@ namespace Calculatrice
             // 
             this.bu_back.BackColor = System.Drawing.SystemColors.ControlDark;
             this.bu_back.Enabled = false;
-            this.bu_back.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_back.Font = new System.Drawing.Font("Quicksand", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_back.Location = new System.Drawing.Point(12, 461);
             this.bu_back.Name = "bu_back";
             this.bu_back.Size = new System.Drawing.Size(96, 44);
@@ -242,7 +243,7 @@ namespace Calculatrice
             // bu_vb
             // 
             this.bu_vb.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.bu_vb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_vb.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_vb.Location = new System.Drawing.Point(290, 0);
             this.bu_vb.Name = "bu_vb";
             this.bu_vb.Size = new System.Drawing.Size(123, 44);
@@ -255,7 +256,7 @@ namespace Calculatrice
             // bu_vc
             // 
             this.bu_vc.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.bu_vc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_vc.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_vc.Location = new System.Drawing.Point(417, 0);
             this.bu_vc.Name = "bu_vc";
             this.bu_vc.Size = new System.Drawing.Size(153, 44);
@@ -280,10 +281,10 @@ namespace Calculatrice
             // l_cst
             // 
             this.l_cst.AutoSize = true;
-            this.l_cst.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_cst.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.l_cst.Location = new System.Drawing.Point(581, 71);
             this.l_cst.Name = "l_cst";
-            this.l_cst.Size = new System.Drawing.Size(46, 18);
+            this.l_cst.Size = new System.Drawing.Size(44, 18);
             this.l_cst.TabIndex = 17;
             this.l_cst.Text = "label1";
             this.l_cst.Visible = false;
@@ -297,10 +298,10 @@ namespace Calculatrice
             // l_virg
             // 
             this.l_virg.AutoSize = true;
-            this.l_virg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_virg.Font = new System.Drawing.Font("Reem Kufi", 9.749999F);
             this.l_virg.Location = new System.Drawing.Point(114, 478);
             this.l_virg.Name = "l_virg";
-            this.l_virg.Size = new System.Drawing.Size(215, 16);
+            this.l_virg.Size = new System.Drawing.Size(210, 25);
             this.l_virg.TabIndex = 26;
             this.l_virg.Text = "Ne rentrez pas de valeurs à virgule";
             this.toolTip1.SetToolTip(this.l_virg, "Je travaille actuellement pour permettre les valeurs à virgule");
@@ -309,7 +310,7 @@ namespace Calculatrice
             // bu_somm
             // 
             this.bu_somm.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.bu_somm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_somm.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_somm.Location = new System.Drawing.Point(0, 50);
             this.bu_somm.Name = "bu_somm";
             this.bu_somm.Size = new System.Drawing.Size(92, 44);
@@ -322,7 +323,7 @@ namespace Calculatrice
             // bu_diff
             // 
             this.bu_diff.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.bu_diff.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_diff.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_diff.Location = new System.Drawing.Point(98, 50);
             this.bu_diff.Name = "bu_diff";
             this.bu_diff.Size = new System.Drawing.Size(98, 44);
@@ -335,7 +336,7 @@ namespace Calculatrice
             // bu_prod
             // 
             this.bu_prod.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.bu_prod.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_prod.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_prod.Location = new System.Drawing.Point(202, 50);
             this.bu_prod.Name = "bu_prod";
             this.bu_prod.Size = new System.Drawing.Size(80, 44);
@@ -348,7 +349,7 @@ namespace Calculatrice
             // bu_quo
             // 
             this.bu_quo.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.bu_quo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bu_quo.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bu_quo.Location = new System.Drawing.Point(288, 50);
             this.bu_quo.Name = "bu_quo";
             this.bu_quo.Size = new System.Drawing.Size(89, 44);
@@ -358,6 +359,17 @@ namespace Calculatrice
             this.bu_quo.UseVisualStyleBackColor = false;
             this.bu_quo.Click += new System.EventHandler(this.bu_quo_Click);
             // 
+            // l_credits
+            // 
+            this.l_credits.AutoSize = true;
+            this.l_credits.Font = new System.Drawing.Font("Quicksand Medium", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_credits.Location = new System.Drawing.Point(369, 478);
+            this.l_credits.Name = "l_credits";
+            this.l_credits.Size = new System.Drawing.Size(256, 16);
+            this.l_credits.TabIndex = 31;
+            this.l_credits.Text = "Programme fait entièrement par Loris F.";
+            this.toolTip1.SetToolTip(this.l_credits, "(il est sacrément fort celui-là)");
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -365,6 +377,7 @@ namespace Calculatrice
             this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.Menu;
             this.ClientSize = new System.Drawing.Size(808, 517);
+            this.Controls.Add(this.l_credits);
             this.Controls.Add(this.bu_quo);
             this.Controls.Add(this.bu_prod);
             this.Controls.Add(this.bu_diff);
@@ -388,6 +401,7 @@ namespace Calculatrice
             this.Controls.Add(this.l_valeur2);
             this.Controls.Add(this.l_valeur1);
             this.Controls.Add(this.bu_aq);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Calculatrice ";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -425,6 +439,7 @@ namespace Calculatrice
         private System.Windows.Forms.Button bu_diff;
         private System.Windows.Forms.Button bu_prod;
         private System.Windows.Forms.Button bu_quo;
+        private System.Windows.Forms.Label l_credits;
     }
 }
 
